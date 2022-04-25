5ebbbc8628aacd48115c5fc52bd9f3194b8a0028
DavisDabols <39349277+DavisDabols@users.noreply.github.com>
2022-04-25 20:14:02 +0300

    Pievienota profila lapa

---
+++ Majaslapa/InvPalMajaslapa/InvPalMajaslapa/Controllers/ProfileController.cs
@@ -0,0 +1,25 @@
+﻿using InvPalMajaslapa.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InvPalMajaslapa.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public ProfileController(UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await userManager.GetUserAsync(User);
+            return View(user);
+        }
+    }
+}
--- Majaslapa/InvPalMajaslapa/InvPalMajaslapa/Models/ApplicationUser.cs
+++ Majaslapa/InvPalMajaslapa/InvPalMajaslapa/Models/ApplicationUser.cs
@@ -4,9 +4,9 @@ namespace InvPalMajaslapa.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string CompanyName { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? CompanyName { get; set; }
         public List<Warehouse> Warehouses { get; set; } = new();
         public List<Worker> Workers { get; set; } = new();
     }
