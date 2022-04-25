cdeee2d18066fc446fb4e35969f707b8e9ee7997
DavisDabols <39349277+DavisDabols@users.noreply.github.com>
2022-04-25 21:16:38 +0300

    Pievienota lietotāju rediģēšana

--- Majaslapa/InvPalMajaslapa/InvPalMajaslapa/Controllers/ProfileController.cs
+++ Majaslapa/InvPalMajaslapa/InvPalMajaslapa/Controllers/ProfileController.cs
@@ -21,5 +21,46 @@ namespace InvPalMajaslapa.Controllers
             var user = await userManager.GetUserAsync(User);
             return View(user);
         }
+
+        //GET
+        public async Task<IActionResult> Edit()
+        {
+            var user = await userManager.GetUserAsync(User);
+            var model = new EditUserViewModel 
+            {
+                Name = user.Name,
+                Surname = user.Surname,
+                CompanyName = user.CompanyName,
+                Email = user.Email
+            };
+            return View(model);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(EditUserViewModel obj)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await userManager.GetUserAsync(User);
+                var updatePassword = await userManager.ChangePasswordAsync(user, obj.OldPassword, obj.Password);
+                if (updatePassword.Succeeded)
+                {
+                    user.Name = obj.Name;
+                    user.Surname = obj.Surname;
+                    user.CompanyName = obj.CompanyName;
+                    user.Email = obj.Email;
+
+                    await userManager.UpdateAsync(user);
+                    return RedirectToAction("index", "profile");
+                }
+                else
+                {
+                    ModelState.AddModelError("OldPassword", "Vecā parole nesakrīt");
+                }
+            }
+            return View(obj);
+        }
     }
 }
---
+++ Majaslapa/InvPalMajaslapa/InvPalMajaslapa/Models/EditUserViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InvPalMajaslapa.Models
+{
+    public class EditUserViewModel
+    {
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? CompanyName { get; set; }
+        [Required(ErrorMessage = "Epasta adrese lauks ir obligāts")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Vecās paroles lauks ir obligāts")]
+        [DataType(DataType.Password)]
+        public string OldPassword { get; set; }
+
+        [Required(ErrorMessage = "Paroles lauks ir obligāts")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Parole")]
+        [MinLength(6, ErrorMessage = "Minimālais paroles garums ir 6 simboli")]
+        [RegularExpression(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(.{6,})$",
+            ErrorMessage = "Parolei nepieciešams vismaz 1 lielais burts un vismaz 1 cipars")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Apstiprināt paroli lauks ir obligāts")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password",
+            ErrorMessage = "Paroles nav vienādas")]
+        public string ConfirmPassword { get; set; }
+    }
+}
