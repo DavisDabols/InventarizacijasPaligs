9ea9f141c4c83f090b5baea94ab7aaebfaf9a66f
DavisDabols <39349277+DavisDabols@users.noreply.github.com>
2022-05-12 12:43:35 +0300

    Izlabota lietu rediģēšana

--- Aplikacija/app/src/main/java/com/davisdabols/inventarizacijaspaligs/ui/edititem/EditItemFragment.kt
+++ Aplikacija/app/src/main/java/com/davisdabols/inventarizacijaspaligs/ui/edititem/EditItemFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.set
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.davisdabols.inventarizacijaspaligs.R
@@ -25,6 +26,9 @@ class EditItemFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.itemTitleInput.setText(viewModel.selectedItem!!.Name)
+        binding.itemDescriptionInput.setText(viewModel.selectedItem!!.Description)
 
         binding.closeEditItems.setOnClickListener {
             openFragment(R.id.navigation_view_item)
